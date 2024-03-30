becac396513948639c3fb73b34b9e7503f5c75a3
AjayxSingh <112817269+AjayxSingh@users.noreply.github.com>
2024-03-30 13:24:02 +0530

    Updated  Event.jsx

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,10 +1,17 @@
-import Defaultlayout from '../components/Defaultlayout';
-import './event.scss';
-export default function Event({selected , setselected}) {
-  setselected("event");
+import React from 'react'
+import { CarouselWithContent } from '.components/Crausel';
+
+function Events() {
+    
   return (
-    <Defaultlayout>
-    <h1>This is Event page</h1>
-    </Defaultlayout>
+    <div className='w-full pb-10'>
+        <div className=' flex flex-col items-center  justify-center font-semibold py-8 '>
+            <h1 className=' text-slate-600 text-lg sm:text-3xl'>Events</h1>
+            <div className=' mt-2 w-1/4 h-[2px] rounden-lg opacity-[30%] bg-green-500 '></div>
+        </div>
+        <CarouselWithContent />
+    </div>
   )
 }
+
+export default Events;
