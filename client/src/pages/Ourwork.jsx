65ff22bec4d980ce07b5004506122f42b744aa30
Indraudh <indraudhsarkar001@gmail.com>
2024-04-12 16:33:03 +0530

    ourwork updated

--- client/src/pages/Ourwork.jsx
+++ client/src/pages/Ourwork.jsx
@@ -1,37 +1,37 @@
 import Defaultlayout from '../components/Defaultlayout';
+import {list1, list2 , list3 , list4 } from '../components/data';
 import './ourwork.scss';
 export default function Ourwork({setselected}) {
   setselected("portfolio");
-  const data=require('../components/data.js');
   return (
     <Defaultlayout>
 <div class="portfolio_header">
   <h1 className='portfolio_title'>PORTFOLIO</h1>
 </div>
-<div class="row"> 
-  <div class="column">
-  {data.list1.map((d)=>(
+<div className="portfolio_row"> 
+  <div className="portfolio_column">
+  {list1.map((d)=>(
      <div className="imgholder">
           <img src={d} alt="" className='portfolio_img' />
      </div>
   ))}
   </div>
-  <div class="column">
-  {data.list2.map((d)=>(
+  <div className="portfolio_column">
+  {list2.map((d)=>(
      <div className="imgholder">
           <img src={d} alt="" className='portfolio_img' />
      </div>
   ))}
   </div>  
-  <div class="column">
-  {data.list3.map((d)=>(
+  <div className="portfolio_column">
+  {list3.map((d)=>(
      <div className="imgholder">
           <img src={d} alt="" className='portfolio_img' />
      </div>
   ))}
   </div>
-  <div class="column">
-  {data.list4.map((d)=>(
+  <div className="portfolio_column">
+  {list4.map((d)=>(
      <div className="imgholder">
           <img src={d} alt="" className='portfolio_img' />
      </div>
